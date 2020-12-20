46f47cb93a3886306acb22bac31852bd9ef5ecd2
FlyebuX <valentin.eberhardt57@gmail.com>
2020-12-20 17:45:16 +0100

    Pendu : interface quasi finie, exceptions et assertions de même ; isdigit() ajouté

--- pendu.py
+++ pendu.py
@@ -13,15 +13,12 @@ def choix_hasard():
     fichier = open("technologies.txt", "r")
     lignes = fichier.readlines()
     for ligne in lignes:
+        mot = choice(lignes)
 
-        a = choice(lignes)
+    liste_eclatee = list(mot)
 
-    liste_eclattee = list(a)
-
-    liste_eclattee.pop()
-    liste_eclattee.pop()
-
-    mot_choisi = "".join(liste_eclattee)
+    del liste_eclatee[-2:]
+    mot_choisi = "".join(liste_eclatee)
 
     return mot_choisi
 
@@ -33,10 +30,18 @@ def strAffiche(ch):
     :return: str : mot_affiche, mot masqué
     """
 
+    # préconditions
+    assert type(ch) == str
+
+
     mot_affiche = ""
     while len(ch) != len(mot_affiche):
         mot_affiche += "*"
-    return mot_affiche
+
+    # postconditions
+    assert len(ch) == len(mot_affiche)
+
+    return mot_affiche  # retour du mot masqué
 
 
 def strReplace(ch1, ch2, lettre):
@@ -57,7 +62,7 @@ def strReplace(ch1, ch2, lettre):
     return ch  # retour du mot modifié
 
 
-def menu(action):
+def menu():
     """
     Menu du jeu du pendu
     :param: action : str : action que l'utilisateur souhaite effectuer
@@ -67,49 +72,59 @@ def menu(action):
     # préconditions
 
     # on vérifie que le chiffre associé à l'action est valide
-    assert action == "1" or action == "2"
 
+    mot_choisi = choix_hasard()
 
+    coups = len(mot_choisi) + 1
 
-    while action != "2":
+    mot_affiche = strAffiche(mot_choisi)
 
-        if action == "1":
+    print(mot_affiche)
 
-            mot_choisi = choix_hasard()
-            mot_affiche = strAffiche(mot_choisi)
+    compteur = 10
+    lettres_proposees = []
 
-            compteur = 10
-            lettres_proposees = []
+    while mot_affiche != mot_choisi and compteur != 0:
+        print("\n-----------------------------")
+        print("Mot inconnu", mot_affiche)
+        print("-----------------------------\n")
+        print("Coup numéro :", compteur)
+        print("Lettres déjà proposées :", lettres_proposees, "")
+        print("Il faut trouver le mot en moins de ", coups, "coups !")
+        print("-------------------------------------------------------------")
 
-            while mot_affiche != mot_choisi:
+        lettre = input("\nSaisir une lettre : ")
 
-                lettre = input("\nSaisir une lettre : ")
+        if lettre.isdigit() is True:
 
-                # si la lettre est contenue mais pas affichée
-                if lettre in mot_choisi and lettre not in mot_affiche:
-                    mot_affiche = strReplace(mot_choisi, mot_affiche, lettre)
-                    compteur -= 1
-                    lettres_proposees.append(lettre)
+            # si la lettre est contenue mais pas affichée
+            if lettre in mot_choisi and lettre not in mot_affiche:
+                mot_affiche = strReplace(mot_choisi, mot_affiche, lettre)
+                lettres_proposees.append(lettre)
 
-                # si la lettre a déjà été saisie mais qu'elle n'est pas contenue
-                elif lettre in lettres_proposees and lettre not in mot_choisi:
-                    print("Lettre déjà proposée !")
-                    compteur -= 1
+            # si la lettre a déjà été saisie mais qu'elle n'est pas contenue
+            elif lettre in lettres_proposees and lettre not in mot_choisi:
+                print("Lettre déjà proposée !")
+                compteur -= 1
 
-                # si la lettre a déjà été saisie et qu'elle est affichée
-                elif lettre in lettres_proposees and lettre in mot_affiche:
-                    print("Lettre déjà proposée et déjà remplacée dans le mot !")
+            # si la lettre a déjà été saisie et qu'elle est affichée
+            elif lettre in lettres_proposees and lettre in mot_affiche:
+                print("Lettre déjà proposée et déjà remplacée dans le mot !")
 
-                # si la lettre n'est pas contenue
-                elif lettre not in mot_choisi:
-                    print("Lettre non contenue dans le mot !")
-                    compteur -= 1
-                    lettres_proposees.append(lettre)
+            # si la lettre n'est pas contenue
+            elif lettre not in mot_choisi:
+                print("Lettre non contenue dans le mot !")
+                compteur -= 1
+                lettres_proposees.append(lettre)
 
+        elif lettre.isdigit() is False:
+            print("Ne pas saisir d'autres caractères que des chiffres !")
 
-                print("Coup numéro :", compteur)
-                print("Mot inconnu", mot_affiche)
-                print("Lettres déjà proposées :", lettres_proposees, "\n")
+    if mot_affiche == mot_choisi:
+        print("Bravo vous avez trouvé ! Le mot était : ", mot_choisi)
+
+    else:
+        print("Perdu ! le mot était : ", mot_choisi, "\nVous pouvez rejouer !")
 
 
 ###########################
@@ -122,9 +137,17 @@ continuer = True
 while continuer != False:
 
     try:
+        action = input("\nBonjour bienvenue sur notre jeu !\n1- Jouer !\n2- Quitter\n")
 
-        action = input("\nBonjour bienvenue sur notre jeu !\n1- Jouer !\n2- Quitter ")
-        menu(action)
+        if action == "1":
+            menu()
+
+        elif action == "2":
+            print("\nA bientôt pour de nouvelles parties endiablées !")
+            continuer = False
+
+        else:
+            raise ValueError
 
     except AssertionError:
         print("Merci de saisir un chiffre valide !")
@@ -133,6 +156,8 @@ while continuer != False:
         print("Vous avez quitté le jeu ! A bientot !")
         continuer = False
 
+    except ValueError:
+        print("Merci de saisir des chiffres valides (1 ou 2) !")
 
 
 
