23ad625af5af69990a1c7b3a5f05fa8335504e2b
FlyebuX <valentin.eberhardt57@gmail.com>
2020-12-20 11:26:16 +0100

    Pendu modifié : ajout de la fonction choix_hasard() et menu quasi fonctionnel // Ajout du fichier texte -thème technologies-

--- pendu.py
+++ pendu.py
@@ -1,12 +1,30 @@
 # Créé par Elève, le 19/12/2020 en Python 3.7
 
+from random import choice  # on importe 'choice' pour choisir aléatoirement un mot parmi une liste de mots
 
 ####################
-### Fonctions ######
+### Fonctions
 ####################
 
+def choix_hasard():
+    """Choisi un mot au hasard dans une liste prédéfinie
+    :return: mot_choisi : str : mot à deviner
+    """
+    fichier = open("technologies.txt", "r")
+    lignes = fichier.readlines()
+    for ligne in lignes:
 
-from random import choice
+        a = choice(lignes)
+
+    liste_eclattee = list(a)
+
+    liste_eclattee.pop()
+    liste_eclattee.pop()
+
+    mot_choisi = "".join(liste_eclattee)
+
+    return mot_choisi
+
 
 def strAffiche(ch):
     """
@@ -23,45 +41,97 @@ def strAffiche(ch):
 
 def strReplace(ch1, ch2, lettre):
     """
-    Remplace les "*" par des lettres
+    Remplace les "*" par des lettres si elles sont contenues dans le mot
     :param: ch1 : str : mot_choisi, c'est le mot à deviner
             ch2 : str : mot_affiche, c'est le mot masqué qui évolue
             lettre : str : lettre saisie par l'utilisateur
-    :return: ch : str : mot masqué a évolué
+    :return: ch : str : mot masqué qui a peut être évolué
     """
 
-    liste_lettres = list(ch2)
+    liste_lettres = list(ch2)  # on transforme le mot masqué en une liste
+
     for i in range(len(ch1)):
-        if ch1[i] == lettre:
-            liste_lettres[i] = lettre
-    ch = "".join(liste_lettres)
-    return ch
+        if ch1[i] == lettre:  # si la lettre est cotenue dans le mot
+            liste_lettres[i] = lettre  # remplacement d'une "*" par la lettre
+    ch = "".join(liste_lettres)  # on recolle les éléments de la liste en une str
+    return ch  # retour du mot modifié
+
+
+def menu(action):
+    """
+    Menu du jeu du pendu
+    :param: action : str : action que l'utilisateur souhaite effectuer
+    """
+
+
+    # préconditions
+
+    # on vérifie que le chiffre associé à l'action est valide
+    assert action == "1" or action == "2"
+
+
+
+    while action != "2":
+
+        if action == "1":
+
+            mot_choisi = choix_hasard()
+            mot_affiche = strAffiche(mot_choisi)
+
+            compteur = 10
+            lettres_proposees = []
+
+            while mot_affiche != mot_choisi:
+
+                lettre = input("\nSaisir une lettre : ")
+
+                # si la lettre est contenue mais pas affichée
+                if lettre in mot_choisi and lettre not in mot_affiche:
+                    mot_affiche = strReplace(mot_choisi, mot_affiche, lettre)
+                    compteur -= 1
+                    lettres_proposees.append(lettre)
+
+                # si la lettre a déjà été saisie mais qu'elle n'est pas contenue
+                elif lettre in lettres_proposees and lettre not in mot_choisi:
+                    print("Lettre déjà proposée !")
+                    compteur -= 1
+
+                # si la lettre a déjà été saisie et qu'elle est affichée
+                elif lettre in lettres_proposees and lettre in mot_affiche:
+                    print("Lettre déjà proposée et déjà remplacée dans le mot !")
+
+                # si la lettre n'est pas contenue
+                elif lettre not in mot_choisi:
+                    print("Lettre non contenue dans le mot !")
+                    compteur -= 1
+                    lettres_proposees.append(lettre)
+
+
+                print("Coup numéro :", compteur)
+                print("Mot inconnu", mot_affiche)
+                print("Lettres déjà proposées :", lettres_proposees, "\n")
 
 
 ###########################
 ### programme principal ###
 ###########################
 
-mot_choisi = "chaise"
-mot_affiche = strAffiche(mot_choisi)
 
-compteur = 10
+continuer = True
 
-while mot_affiche != mot_choisi:
+while continuer != False:
 
-    lettre = input("Saisir une lettre :")
+    try:
 
-    if lettre in mot_choisi:
-        mot_affiche = strReplace(mot_choisi, mot_affiche, lettre)
-        compteur -= 1
+        action = input("\nBonjour bienvenue sur notre jeu !\n1- Jouer !\n2- Quitter ")
+        menu(action)
 
-    elif lettre not in mot_choisi:
-        print("Lettre non contenue dans le mot !")
-        compteur -= 1
+    except AssertionError:
+        print("Merci de saisir un chiffre valide !")
 
-    print(mot_affiche)
-
-
+    except KeyboardInterrupt:
+        print("Vous avez quitté le jeu ! A bientot !")
+        continuer = False
 
 
 
